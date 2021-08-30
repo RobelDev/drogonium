600e3c29c74312748515492a153cd8534ce3b006
RobelDev <Robelktegegne@gmail.com>
2021-08-30 15:06:17 -0700

    A signle block instance

--- src/blockchain/block.cpp
+++ src/blockchain/block.cpp
@@ -2,6 +2,8 @@
 #include <ctime>
 #include <string>
 #include <map>
+// #include <drogon/HttpSimpleController.h>
+//jsoncpp::Json::Value res;
 
 using namespace std;
 
@@ -9,9 +11,9 @@ class Block
 {
 public:
     size_t index;
-    time_t timestamp;
+    size_t timestamp;
     size_t nonce;
-    unsigned short difficulty;
+    size_t difficulty;
     string previous_hash;
     string hash;
     string data;
@@ -30,16 +32,51 @@ public:
         this->data = data;
     }
 
-    // //void serialize()
-    // {
-    //     cout << "block serialize" << endl;
-    //     // return 0;
-    //     // return "hello";
-    // }
+    static string mine_block(Block previous_block, string data)
+    {
+        return "mine_block_fun";
+    }
+
+    static Block create_genesis()
+    {
+        return Block(0, 0, 1, 1,
+                     "genesis_prev_hash", "genesis_hash", "genesis_data");
+    }
+
+    //to-do
+    static bool is_valid_block()
+    {
+        return false;
+    }
 };
 
-int main()
+// overload the operator "<<". and the left side is cout
+// which is ostream& output reference, and the object of Block block
+ostream &
+operator<<(ostream &output, Block block)
 {
+    output << "index : " << block.index << endl
+           << "timestamp : " << block.timestamp << endl
+           << "nonce : " << block.nonce << endl
+           << "difficulty : " << block.difficulty << endl
+           << "previous_hash : " << block.previous_hash << endl
+           << "hash : " << block.hash << endl
+           << "data : " << block.data << endl;
+
+    return output;
+};
+
+int main1()
+{
+
     Block block(0, 1, 4, 1, "prev", "hash", "data");
-    cout << "check num" << block.nonce << endl;
+
+    cout << "block" << block << endl;
+
+    cout << "------------------------------------" << endl;
+
+    cout << "gennesis block: " << (Block::create_genesis()) << endl;
+
+    cout << "------------------------------------" << endl;
+    return 0;
 }
